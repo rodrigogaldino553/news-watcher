2489fce3b245b6d78e2f5ded4360dc82c0974a5f
rodrigogaldino553 <63233782+rodrigogaldino553@users.noreply.github.com>
2021-02-10 23:00:24 -0200

    all right

--- src/database/db-interactions.js
+++ src/database/db-interactions.js
@@ -2,6 +2,27 @@ const db = require('./db-config')
 
 
 const interactions = {
+
+    async createNews(news) {
+        news.illustration = !news.illustration ? news.illustration = 'https://image.flaticon.com/icons/png/512/14/14711.png' : news.illustration;
+        try {
+            const connection = await db.connect()
+            const sql = 'INSERT INTO news(title, content, illustration, category, views) values (?,?,?,?,?);'
+            const values = [news.title, news.content, news.illustration, news.category, 0]
+
+            await connection.query(sql, values)
+            let [lastID] = await connection.query('SELECT id FROM news WHERE title=?', news.title)
+            let position = lastID.length - 1
+            lastID = lastID[position].id
+
+            this.createCategory(news.category, lastID)
+            return [true, lastID]
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+    },
+
     async selectAll() {
         try {
             const connection = await db.connect()
@@ -32,27 +53,10 @@ const interactions = {
 
     async selectNewsByCategory(category) {
         const connection = await db.connect()
-        const sql = ``
+        const sql = `SELECT news_category.*, news.* FROM news JOIN news_category ON (news_category.news_id = ${category}) WHERE news_category.news_id = ${category};`
 
     },
 
-    async createNews(news) {
-        try {
-            const connection = await db.connect()
-            const sql = 'INSERT INTO news(title, content, illustration, category, views) values (?,?,?,?,?);'
-            const values = [news.title, news.content, news.illustration, news.category, 0]
-
-            await connection.query(sql, values)
-            let [lastID] = await connection.query('SELECT id FROM news WHERE title=?', news.title)
-            let position = lastID.length - 1
-            lastID = lastID[position].id
-
-            this.createCategory(news.category, lastID)
-            return [true, lastID]
-        } catch (error) {
-            return false
-        }
-    },
 
     async updateNews() {
 
@@ -64,20 +68,29 @@ const interactions = {
 
     async createCategory(categoryName, newsID) {
         try {
+            console.log(`${categoryName}, ${newsID}`)
             const connection = await db.connect()
             const sql = 'INSERT INTO news_category(category, news_id) VALUES(?, ?);'
             await connection.query(sql, [categoryName, newsID])
 
-            return 1
+            return true
         } catch (error) {
-            return 0
+            console.log(error)
+            return false
         }
     },
 
     async selectAllCategories(categoryName) {
-        const connection = await db.connect()
-        const [rows] = await connection.query('SELECT * FROM news_category;')
-        return rows
+        try {
+            const connection = await db.connect()
+            const [rows] = await connection.query('SELECT * FROM news_category;')
+            return rows
+
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+
     }
 }
 
